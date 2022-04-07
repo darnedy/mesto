1cdf93a0e6a95d9a02f6f5e5389104afa012a607
darnedy <darnedy@yandex-team.ru>
2022-04-07 21:15:14 +0300

    fix_first_review_1

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,35 @@ const popup = document.querySelector('.popup');
 const openPopup = document.querySelector('.profile__edit-button');
 const closePopup = popup.querySelector('.popup__close');
 
-function togglePopup() {
-    popup.classList.toggle('popup__open') 
-};
-
-openPopup.addEventListener('click',togglePopup);
-
-closePopup.addEventListener('click',togglePopup);
-
-
 let formElement = popup.querySelector('.popup__container');
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let descriptionInput = formElement.querySelector('.popup__input_description');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+
+// function togglePopup() {
+//     popup.classList.toggle('popup_opened') 
+// };
+
+// openPopup.addEventListener('click',togglePopup);
+
+// closePopup.addEventListener('click',togglePopup);
+
+function popupOpen() {
+    popup.classList.add('popup_opened');
+  
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+  }
+  
+  function popupClose() {
+    popup.classList.remove('popup_opened');
+  }
+
+
+// let formElement = popup.querySelector('.popup__container');
+// let nameInput = formElement.querySelector('.popup__input_type_name');
+// let descriptionInput = formElement.querySelector('.popup__input_description');
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -27,16 +44,18 @@ function formSubmitHandler (evt) {
     let description = descriptionInput.value;
 
     // Выберите элементы, куда должны быть вставлены значения полей
-    let profileName = document.querySelector('.profile__name');
-    let profileDescription = document.querySelector('.profile__description');
+    // let profileName = document.querySelector('.profile__name');
+    // let profileDescription = document.querySelector('.profile__description');
 
     // Вставьте новые значения с помощью textContent
     profileName.textContent = name;
     profileDescription.textContent = description;
 
-    togglePopup();
+    popupClose();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler); 
+openPopup.addEventListener('click',popupOpen);
+closePopup.addEventListener('click',popupClose);
